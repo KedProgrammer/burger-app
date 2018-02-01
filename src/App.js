b9d253ee2679f84f2dfb8b3d898e155b61622c18
daniel <tuproveedoronlineco@gmail.com>
2018-02-01 01:22:13 -0500

    add checkout component2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <div>
         <Layout>
           <Route path="/" component={BurgerBuilder} />
-            <Route path="/checkout" component={Checkout} />
+          <Route path="/checkout" component={Checkout} />
         </Layout>
       </div>
     );
