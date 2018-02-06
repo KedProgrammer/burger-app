0b9c4ffec743bbe5b671f1d865858a09790cd5ef
daniel <tuproveedoronlineco@gmail.com>
2018-02-06 18:38:23 -0500

    checkout component

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,11 +3,18 @@ import PropTypes from 'prop-types'
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
 class Checkout  extends Component {
 
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
+  }
+
+  checkoutContinuedHandler = () => {
+    this.props.history.replace('/checkout/contact-data')
+  }
 
   render () {
     return (
       <div>
-        <CheckoutSummary  />
+        <CheckoutSummary checkoutCancelled={this.checkoutCancelledHandler} checkoutContinued={this.checkoutContinuedHandler}  />
       </div>
     )
   }
