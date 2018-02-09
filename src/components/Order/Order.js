d7e65cd54633f68d8bf1c169d7e0433135073857
daniel tobon <shiriux1@gmail.com>
2018-02-09 16:27:48 -0500

    add order

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import classes from './Order.css'
 
 
+const order = (props) => {
+	let order = null;
+	if (props.ingredients) {
+		order = Object.keys(props.ingredients).map(key => (
+					<span
+						style={{
+							textTransform: 'capitalize',
+							display: 'inline-block',
+							margin: '0 8px',
+							border: '1px solid #ccc',
+							padding: '6px'
+						}}
+					>{key}: {props.ingredients[key]}</span>
 
-const order = (props) => (
-		<div>
+				))
+	}
+	return (
+		<div className={classes.Order}>
 			<p>Ingredients: </p>
-			<p>Price: </p>
+				{order}
+			<p>Price: <strong>USD {props.price}</strong></p>
 		</div>
-
-
-	);
+	)
+}
 
 export default order;
